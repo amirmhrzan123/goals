48ef128a44508ab50f8e13d7f6a305995f2e8124
Amir <amir.maharjan@ebpearls.com>
2017-10-09 16:29:46 +0545

    floating action added

---
+++ app/src/main/java/com/example/amir/goalapp/Utils.java
@@ -0,0 +1,24 @@
+package com.example.amir.goalapp;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+
+/**
+ * Created by Amir on 09/10/2017.
+ */
+
+public class Utils {
+
+    public static int getToolbarHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                new int[]{R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
+    }
+
+    public static int getTabsHeight(Context context) {
+        return (int) context.getResources().getDimension(R.dimen.tabsHeight);
+    }
+}
